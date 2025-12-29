298b0b78bd1309164dcb737e31ac52e68563c2f0
kimtrinh11964-lang <kimtrinh11964@gmail.com>
2025-12-29 11:56:06 +0700

    Create monthly_sales_june_2014.sql

---
+++ queries/monthly_sales_june_2014.sql
@@ -0,0 +1,39 @@
+-- Business Question:
+-- What was the sales performance in June 2014?
+
+WITH TotalSalesAmount AS(
+	SELECT 
+		P.ProductID,
+		P.Name 'Product',
+		PC.Name 'Category',
+		PS.Name 'Subcategory',
+		SUM(SOD.OrderQty) 'TotalSalesQuantity',
+		SUM(SOD.LineTotal) 'TotalSalesAmount'
+	FROM Sales.SalesOrderHeader SOH
+	LEFT JOIN Sales.SalesOrderDetail SOD
+	ON SOH.SalesOrderID=SOD.SalesOrderID
+	LEFT JOIN Production.Product P
+	ON P.ProductID=SOD.ProductID
+	LEFT JOIN Production.ProductSubcategory PS
+	ON PS.ProductSubcategoryID=P.ProductSubcategoryID
+	LEFT JOIN Production.ProductCategory PC
+	ON PC.ProductCategoryID=PS.ProductCategoryID
+	WHERE MONTH(SOH.OrderDate)=6
+	AND YEAR(SOH.OrderDate)=2014
+	GROUP BY 
+		P.ProductID,
+		P.Name ,
+		PC.Name,
+		PS.Name )
+SELECT 
+	ProductID,
+	Product,
+	Category,
+	Subcategory,
+	TotalSalesQuantity,
+	SUM(TotalSalesQuantity) OVER (PARTITION BY Category) 'TotalCatSalesQuantity',
+	TotalSalesAmount,
+	SUM(TotalSalesAmount) OVER (PARTITION BY Category) 'TotalCatSalesAmount'
+FROM TotalSalesAmount 
+ORDER BY 
+	TotalSalesAmount DESC
